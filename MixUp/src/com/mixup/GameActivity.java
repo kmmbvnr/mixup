966b0317f738dca71415a25fd9a1accd9d3e8776
YOUR NAME <YOUR_EMAIL@aldigit.com>
2012-02-18 19:18:57 +0700

    Added who_am_i button

--- MixUp/src/com/mixup/GameActivity.java
+++ MixUp/src/com/mixup/GameActivity.java
@@ -2,6 +2,9 @@ package com.mixup;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class GameActivity extends FragmentActivity {
 
@@ -11,5 +14,12 @@ public class GameActivity extends FragmentActivity {
      
         setContentView(R.layout.game);
 
+        final Button button = (Button) findViewById(R.id.who_am_i_button);
+        button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			//	button.setSelected(true);
+			}
+        });
     }
 }
